3f7bb1c3938f776ad4716bf279230db8f6423904
smn-hrtzsch <simon@hoertzsch.de>
2025-04-24 02:16:56 +0200

    remove vscode extensions

--- vscode/.vscode/extensions/jebbs.plantuml-2.18.1/out/src/plantuml/intellisense/languageCompletion/shared.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REG_CLEAN_LABEL = void 0;
-exports.REG_CLEAN_LABEL = /[^0-9a-z_]/ig;
-//# sourceMappingURL=shared.js.map
--- vscode/.vscode/extensions/pycom.pymakr-2.22.5/src/globals.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="probs" />
-/// <reference types="serialport" />
-/// <reference types="vscode" />
